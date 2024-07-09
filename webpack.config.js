1af4ba40caf2d76b5e42f6f748090d735078c7bd
Sergei <40200071+Sergei1790@users.noreply.github.com>
2024-07-09 13:34:24 +0300

    new

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  mode: 'development',
+  // entry: {
+  //   index: './src/index.js',
+  //   print: './src/print.js',
+  // },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  // devServer: {
+  //   static: './dist',
+  // },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+      minify: false,
+    }),
+  ],
+  // optimization: {
+  //   runtimeChunk: 'single',
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
+
--- webpack.prod.js
+++
@@ -1,8 +0,0 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-
-module.exports = merge(common, {
-  mode: 'production',
-  // if error occurs shows in which file. Without this if we have a lot of files, error will be shown in bundled file
-  devtool: 'source-map',
-});
