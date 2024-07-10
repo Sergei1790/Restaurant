4f02d8e09d7708c00141495db85a75b1a98ffe3c
Sergei <40200071+Sergei1790@users.noreply.github.com>
2024-07-10 13:15:57 +0300

    new

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 import '../css/style.css';
 import {menuPageLoad, contactPageLoad} from './inner-pages';
-import sss from './home-page';
+import homePageLoad from './home-page';
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
 }
 const content = document.getElementById('content');
 const buttons = document.querySelectorAll('.btns');
 buttons.forEach(button => button.addEventListener('click', changePage))
-
+homePageLoad();
 function changePage(event) {
     const buttonId = event.target.id;
     // content.innerHTML ='';
@@ -15,7 +15,7 @@ function changePage(event) {
     console.log(buttonId);
     switch (buttonId) {
         case 'homePageLoad':
-            sss()
+            homePageLoad();
             break;
         case 'menuPageLoad':
             menuPageLoad();
